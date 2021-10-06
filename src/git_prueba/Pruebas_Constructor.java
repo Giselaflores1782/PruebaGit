02a9d214253d15afc427a11f37b54616b34c6a4d
Giselaflores1782 <gisflores@alumno.unlam.edu.ar>
2021-10-05 21:26:24 -0300

    agrego variable 4

--- src/git_prueba/Pruebas_Constructor.java
+++ src/git_prueba/Pruebas_Constructor.java
@@ -5,10 +5,12 @@ public class Pruebas_Constructor {
 	
 	private Integer var1;
 	private Integer var2;
-	
+	private String var3;
+	private String var4;
 	
 	public Pruebas_Constructor(Integer var1, Integer var2) {
 		this.var1=var1;
 		this.var2=var2;
+		
 	}
 }
